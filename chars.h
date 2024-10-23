84235e99b66717a09fb12a5c0d671ddd0e8dcd9f
Jan Ancuta <jas.ancuta@gmail.com>
2024-10-23 07:59:09 +0200

    konwersja z wartości na znaki w liczbach

--- chars.h
+++ chars.h
@@ -1,3 +1,17 @@
-char c[] = {
-    0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'
+#ifndef CHARS_H
+#define CHARS_H
+
+char chars[] = {
+    '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'
 };
+
+int num(char c){
+    if(c <= '9') return c - '0';
+    else return c - 'A' + 10;
+}
+
+char chr(int i){
+    return chars[i];
+}
+
+#endif
